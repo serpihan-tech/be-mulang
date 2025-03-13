b209cb0a4b056e22f6768c1abc476ace72898210
RizalAnas00 <belangsumerlang@gmail.com>
2025-03-13 20:01:21 +0700

    add: install rate limiter

--- adonisrc.ts
+++ adonisrc.ts
@@ -40,6 +40,7 @@ export default defineConfig({
     () => import('@adonisjs/bouncer/bouncer_provider'),
     () => import('@adonisjs/mail/mail_provider'),
     () => import('@adonisjs/transmit/transmit_provider'),
+    () => import('@adonisjs/limiter/limiter_provider')
   ],
 
   /*
