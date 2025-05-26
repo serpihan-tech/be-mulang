6db69650e9a67f0001e8b2fd0f7fc1be81854b05
RizalAnas00 <belangsumerlang@gmail.com>
2025-05-26 09:55:47 +0700

    fix: announcement_schedule date doesnt match and error row not found if there is no admin or teacher in relationship

--- app/schedules/announcement_schedules.ts
+++ app/schedules/announcement_schedules.ts
@@ -3,37 +3,33 @@ import AnnouncementByTeacher from '#models/announcement_by_teacher'
 import { DateTime } from 'luxon'
 import transmit from '@adonisjs/transmit/services/main'
 import Admin from '#models/admin'
-import User from '#models/user'
 import Teacher from '#models/teacher'
 
 export default class AnnouncementSchedules {
-  private static now =
-    DateTime.now().setZone('Asia/Jakarta').toSQL() ??
-    new Date().toISOString().slice(0, 19).replace('T', ' ')
+  private static now = DateTime.local().toISODate()
 
   static async postAnnouncementByAdmin() {
     const announcements = await AnnouncementByAdmin.query().where('date', '=', this.now)
 
     const role = 'admin'
-    let adminId: number = 0
-
-    const admin = await Admin.query().where('id', adminId).preload('user').firstOrFail()
 
     for (const an of announcements) {
-      an.adminId = adminId
+      const admin = await Admin.query().where('id', an.adminId).preload('user').first()
 
-      const t = transmit.broadcast(`notifications/${an.targetRoles}`, {
+      if (!admin) continue
+
+      transmit.broadcast(`notifications/${an.targetRoles}`, {
         message: {
           id: `44${an.id}`,
           title: an.title,
           content: an.content,
           category: an.category,
-          from: admin.name,
+          from: admin.name ?? 'undefined',
           role: role,
           files: an.files,
           date: an.date.toISOString(),
-          senderPicture: admin.profilePicture,
-          senderEmail: admin.user.email,
+          senderPicture: admin.profilePicture ?? 'undefined',
+          senderEmail: admin.user.email ?? 'undefined',
           createdAt: an.createdAt.toString(),
           updatedAt: an.updatedAt.toString(),
         },
@@ -49,20 +45,23 @@ export default class AnnouncementSchedules {
       const teacher = await Teacher.query()
         .where('id', an.teacherId)
         .preload('user', (u) => u.select('id', 'name'))
-        .firstOrFail()
+        .first()
 
-      const t = transmit.broadcast(`notifications/teachers/class/${an.classId}`, {
+      if (!teacher) continue
+
+      // console.log('announcements teacher : ', an)
+      transmit.broadcast(`notifications/teachers/class/${an.classId}`, {
         message: {
           id: `74${an.id}`,
           title: an.title,
           content: an.content,
           category: an.category,
-          from: teacher.name,
+          from: teacher.name ?? 'undefined',
           role: role,
           files: an.files,
           date: an.date.toISOString(),
-          senderPicture: teacher.profilePicture,
-          senderEmail: teacher.user.email,
+          senderPicture: teacher.profilePicture ?? 'undefined',
+          senderEmail: teacher.user.email ?? 'undefined',
           createdAt: an.createdAt.toString(),
           updatedAt: an.updatedAt.toString(),
         },
